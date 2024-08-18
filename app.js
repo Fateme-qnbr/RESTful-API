fa1b80b5defe4f8a1263a067d10fddac5e0e427d
Fateme_qnbr <fatemeqanbari010@gmail.com>
2024-08-18 19:28:00 +0330

    nodemon , morgan

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const orderRoutes =require('./api/routes/orders');
 const productRoutes = require('./api/routes/products');
+
+app.use(morgan('dev'));
 
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
